5befd818c43bcc9560c9d7429549dea0298873cc
Dzulfikri29 <35086896+Dzulfikri29@users.noreply.github.com>
2023-09-23 05:18:25 +0700

    remove gtag js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,20 +5,6 @@
 <html lang="en">
 
 <head>
-
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HW2GCQHLHL"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-HW2GCQHLHL');
-    </script>
-
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title') - {{ $general->website_name }}</title>
